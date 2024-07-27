cd3b9267d2acb4e221e1a65fae73bc690d310348
Kushal8616 <Kanjanap@flemingcollege.ca>
2024-07-26 22:38:13 -0400

    ok
    ok

--- create_relationships.py
+++ create_relationships.py
@@ -5,54 +5,89 @@ Description:
 
 Usage:
  python create_relationships.py
+ python -c "import sys; print(sys.executable)"
 """
+
+
 import os
+
 import sqlite3
 from random import randint, choice
 from faker import Faker
 
-# Connect to the database
 con = sqlite3.connect('social_network.db')
-cur = con.cursor()
+curs = con.cursor()
 
-# SQL query to create the relationships table
-create_relationships_tbl_query = """
-    CREATE TABLE IF NOT EXISTS relationships (
-        id INTEGER PRIMARY KEY,
-        person1_id INTEGER NOT NULL,
-        person2_id INTEGER NOT NULL,
-        type TEXT NOT NULL,
-        start_date DATE NOT NULL,
-        FOREIGN KEY (person1_id) REFERENCES people (id),
-        FOREIGN KEY (person2_id) REFERENCES people (id)
-    );
+# Determine the path of the database
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, 'social_network.db')
+
+    # TODO: Function body
+    # Hint: See example code in lab instructions entitled "Add the Relationships Table"
+create_relationships_table_query = """
+ CREATE TABLE IF NOT EXISTS relationships
+ (
+ id INTEGER PRIMARY KEY,
+ person1_id INTEGER NOT NULL,
+ person2_id INTEGER NOT NULL,
+ type TEXT NOT NULL,
+ start_date DATE NOT NULL,
+ FOREIGN KEY (person1_id) REFERENCES people (id),
+ FOREIGN KEY (person2_id) REFERENCES people (id)
+ );
 """
-
-# Execute the SQL query
-cur.execute(create_relationships_tbl_query)
-
-# Prepare to populate the table with fake data
-fake = Faker()
-add_relationship_query = """
-    INSERT INTO relationships (
-        person1_id,
-        person2_id,
-        type,
-        start_date
-    ) VALUES (?, ?, ?, ?);
-"""
-
-# Insert 100 fake relationships
-for _ in range(100):
-    person1_id = randint(1, 200)
-    person2_id = randint(1, 200)
-    while person2_id == person1_id:
-        person2_id = randint(1, 200)
-    rel_type = choice(['friend', 'spouse', 'partner', 'relative'])
-    start_date = fake.date_between(start_date='-50y', end_date='today')
-    cur.execute(add_relationship_query, (person1_id, person2_id, rel_type, start_date))
-
-# Commit changes and close connection
+# Execute the SQL query to create the 'relationships' table.
+curs.execute(create_relationships_table_query)
 con.commit()
+
+
+
+# TODO: Function body
+# Hint: See example code in lab instructions entitled "Populate the Relationships Table"
+add_relationship_query = """
+INSERT INTO relationships
+( 
+person1_id,
+person2_id,
+type,
+start_date
+)
+VALUES (?, ?, ?, ?);
+"""
+fake = Faker()
+# Randomly select first person in relationship
+person1_id = randint(1, 200)
+# Randomly select second person in relationship
+# Loop ensures person will not be in a relationship with themself
+person2_id = randint(1, 200)
+while person2_id == person1_id:
+    person2_id = randint(1, 200)
+# Randomly select a relationship type
+rel_type = choice(('friend', 'spouse', 'partner', 'relative'))
+# Randomly select a relationship start date between now and 50 years ago
+start_date = fake.date_between(start_date='-50y', end_date='today')
+# Create tuple of data for the new relationship
+new_relationship = (person1_id, person2_id, rel_type, start_date)
+# Add the new relationship to the DB
+curs.execute(add_relationship_query, new_relationship)
+con.commit()
+
+
+# SQL query to get all relationships
+all_relationships_query = """
+SELECT person1.name, person2.name, start_date, type FROM relationships
+JOIN people person1 ON person1_id = person1.id
+JOIN people person2 ON person2_id = person2.id;
+"""
+# Execute the query and get all results
+curs.execute(all_relationships_query)
+
+all_relationships = curs.fetchall()
 con.close()
+
+# Print sentences describing each relationship
+for person1, person2, start_date, type in all_relationships:
+    print(f'{person1} has been a {type} of {person2} since {start_date}.')
+
+
 
--- marriage_report.py
+++ marriage_report.py
@@ -13,19 +13,3 @@ create_people_tbl_query = """
     );
 """
 
-# Execute the SQL query
-cur.execute(create_people_tbl_query)
-
-# Prepare to populate the table with fake data
-fake = Faker()
-add_person_query = """
-    INSERT INTO people (name) VALUES (?);
-"""
-
-# Insert 200 fake people
-for _ in range(200):
-    cur.execute(add_person_query, (fake.name(),))
-
-# Commit changes and close connection
-con.commit()
-con.close()
